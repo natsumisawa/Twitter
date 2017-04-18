33c30f5a2cb759561888b8d7034daf2b5078de41
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2017-04-18 22:00:28 +0900

    init colorbox

---
+++ ColorBox/src/test/java/javatry/enjoy/ikezaki/IkezakiEx1StreamNumberTest.java
@@ -0,0 +1,26 @@
+package javatry.enjoy.ikezaki;
+
+import javatry.colorbox.unit.ColorBoxTestCase;
+
+/**
+ * 数値関連のテスト。<br>
+ * 何々は？と言われたら、それに該当するものをログに出力すること。
+ * @author ikezaki
+ */
+public class IkezakiEx1StreamNumberTest extends ColorBoxTestCase {
+
+    // ===================================================================================
+    //                                                                           Good Luck
+    //                                                                           =========
+    /**
+     * 青色のカラーボックスに入ってる Map の中の商品で一番高いものは？
+     */
+    public void test_findMax() {
+    }
+
+    /**
+     * カラーボックスの中に入ってる BigDecimal を全て足し合わせると？
+     */
+    public void test_sumBigDecimal() {
+    }
+}
