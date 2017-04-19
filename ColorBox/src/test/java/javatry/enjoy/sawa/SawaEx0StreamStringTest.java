34a0df162e7fa5f2eccb9f578618540802a6a697
澤 なつみ <natsumi.sawa@BIZ0015000350.local>
2017-04-19 15:45:22 +0900

    init sawa colorbox

---
+++ ColorBox/src/test/java/javatry/enjoy/sawa/SawaEx0StreamStringTest.java
@@ -0,0 +1,39 @@
+package javatry.enjoy.sawa;
+
+import javatry.colorbox.ColorBox;
+import javatry.colorbox.unit.ColorBoxTestCase;
+import java.util.List;
+
+/**
+ * 文字列のテスト。<br>
+ * 何々は？と言われたら、それに該当するものをログに出力すること。
+ * @author sawa
+ */
+public class SawaEx0StreamStringTest extends ColorBoxTestCase {
+
+
+    // ===================================================================================
+    //                                                                            length()
+    //                                                                            ========
+    /**
+     * カラーボックスに入ってる文字列の中で、一番長い文字列は？
+     */
+    public void test_length_findMax() {
+
+    }
+
+    /**
+     * カラーボックスに入ってる文字列の長さの合計は？
+     */
+    public void test_length_calculateLengthSum() {
+    }
+    
+    // ===================================================================================
+    //                                                            startsWith(), endsWith()
+    //                                                            ========================
+    /**
+     * 「かまくら」で始まる文字列をしまっているカラーボックスの色は？
+     */
+    public void test_startsWith_findFirstWord() {
+    }
+}
