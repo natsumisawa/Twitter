c2916497aa8dc0c820e7790c18ee3ef773e215eb
Ryo-chin <h031203yama@gmail.com>
2017-04-30 18:30:27 +0900

    Q1NumberTestをレビュー！まだまだいけるぞ！

--- ColorBox/src/test/java/javatry/enjoy/sawa/SawaQ1NumberTest.java
+++ ColorBox/src/test/java/javatry/enjoy/sawa/SawaQ1NumberTest.java
@@ -37,9 +37,11 @@ public class SawaQ1NumberTest extends ColorBoxTestCase {
                 }
             }
         }
+        // TODO sawa これだとログだけ見たときに, 何を出力しているのかわかりづらいかなぁ。もう少し丁寧にログを書いてみましょう！ by hakiba (2017/04/30)
         log(monthSum);
     }
 
+    // TODO sawa JavaDocの@throwsでWarning!多分もともとなのだけど, 修正しましょう！ -> 「@throwsの一行を削除する」 or 「どんなときに例外が投げられるかを書いてあげる」 by hakiba (2017/04/30)
     /**
      * カラーボックの中に入っている、0~100までの数値の数は？
      *
@@ -52,13 +54,16 @@ public class SawaQ1NumberTest extends ColorBoxTestCase {
             List<BoxSpace> spaceList = colorBox.getSpaceList();
             for (BoxSpace boxSpace : spaceList) {
                 Object contents = boxSpace.getContents();
+                // TODO sawa 「数値」は Integer だけですか？他の「数値」も考慮したコードにしてみましょう！ by hakiba (2017/04/30)
                 if (contents instanceof Integer) {
+                    // TODO sawa さすがにこの場合の「(Integer) contents」は一旦変数に置き換えてあげてほうが見やすいかな！ by sawa (2017/04/30)
                     if ((0 <= (Integer) contents) && ((Integer) contents <= 100)) {
                         count++;
                     }
                 }
             }
         }
+        // TODO sawa ここも、もう少し丁寧にログを書いてみましょう！ by hakiba (2017/04/30)
         log(count);
     }
 
@@ -79,7 +84,9 @@ public class SawaQ1NumberTest extends ColorBoxTestCase {
                     if (contents instanceof Map<?, ?>) {
                         int maxValue = 0;
                         String product = "";
+                        // TODO sawa 全体的に言えることだけど, 「e」っていう変数名は何を表していますか？ by hakiba (2017/04/30)
                         for (Map.Entry<?, ?> e : ((Map<?, ?>) contents).entrySet()) {
+                            // TODO sawa このままだとMapのValueが Integer じゃなかったとき落ちる気がするよ！落ちないように修正しましょう！ by hakiba (2017/04/30)
                             if (maxValue < (Integer) e.getValue()) {
                                 maxValue = ((Integer) e.getValue());
                                 product = (String) e.getKey();
@@ -97,12 +104,14 @@ public class SawaQ1NumberTest extends ColorBoxTestCase {
      */
     public void test_findColorBigWidthHasInteger() {
         List<ColorBox> colorBoxList = getColorBoxList();
+        // TODO sawa maxとmaxBoxはちょっとわかりづらい。変数名を変えてみよう！ by hakiba (2017/04/30)
         int max = 0;
         String maxBox = "";
         for (ColorBox colorBox : colorBoxList) {
             List<BoxSpace> spaceList = colorBox.getSpaceList();
             for (BoxSpace e : spaceList) {
                 Object contents = e.getContents();
+                // TODO sawa And条件を使ってif文のネストを一段減らしてみよう！（ちなみにifの条件は手前から判定されるよ） by hakiba (2017/04/30)
                 if (contents instanceof Integer) {
                     if (max < colorBox.getSize().getWidth()) {
                         max = colorBox.getSize().getWidth();
@@ -111,6 +120,7 @@ public class SawaQ1NumberTest extends ColorBoxTestCase {
                 }
             }
         }
+        // TODO sawa Integerを持っているカラーボックスが存在しなかった場合どうなる？その場合も考慮してログ表示してみよう！ by hakiba (2017/04/30)
         log("一番幅が大きいものでInteger型を持っているカラーボックスの色は" + maxBox + "です");
     }
 
@@ -125,6 +135,7 @@ public class SawaQ1NumberTest extends ColorBoxTestCase {
             for (BoxSpace e : spaceList) {
                 Object contents = e.getContents();
                 if (contents instanceof List) {
+                    // TODO sawa さすがに「ob」は「obj」にしましょう！ by hakiba (2017/04/30)
                     for (Object ob: (List)contents) {
                         if (ob instanceof BigDecimal) {
                             sum = sum.add((BigDecimal) ob);
@@ -133,6 +144,7 @@ public class SawaQ1NumberTest extends ColorBoxTestCase {
                 }
             }
         }
+        // TODO sawa インデント揃えて！基本中の基本やで！ by hakiba (2017/04/30)
      log("カラーボックスの中に入ってる BigDecimal を全て足し合わせると" + sum + "です");
     }
 }
