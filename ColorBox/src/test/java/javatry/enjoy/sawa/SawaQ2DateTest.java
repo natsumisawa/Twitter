9248b67450374ebf2e6415114dbcfe0b1de00211
natsumi.sawa <natsumi.sawa@bizreach.co.jp>
2017-05-01 18:34:17 +0900

    edit dateTest

--- ColorBox/src/test/java/javatry/enjoy/sawa/SawaQ2DateTest.java
+++ ColorBox/src/test/java/javatry/enjoy/sawa/SawaQ2DateTest.java
@@ -21,8 +21,9 @@ import static java.util.Locale.JAPAN;
  * 何々は？と言われたら、それに該当するものをログに出力すること。
  * @author ikezaki
  */
-// TODO sawa @author by yuki.wakisaka (2017/04/30)
-// TODO sawa おーい by yuki.wakisaka (2017/05/01)
+// TODO done sawa @author by yuki.wakisaka (2017/04/30)
+// TODO done sawa おーい by yuki.wakisaka (2017/05/01)
+// TODO [コメント] レビューありがとうございます by sawa (2017/05/01)
 public class SawaQ2DateTest extends ColorBoxTestCase {
 
     // ===================================================================================
@@ -33,27 +34,30 @@ public class SawaQ2DateTest extends ColorBoxTestCase {
      * カラーボックスに入っている日付をスラッシュ区切りのフォーマットで表示したら？
      */
     public void test_convert() {
-        // TODO sawa colorBoxListはforの引数でしか呼ばれないので、変数に出さずに書いてみよう by yuki.wakisaka (2017/04/30)
-        // TODO [コメント] このTODOだけどうしてもわかりませんでした、一旦飛ばします by sawa
-        List<ColorBox> colorBoxList = getColorBoxList();
-        for (ColorBox colorBox : colorBoxList) {
+        // TODO done sawa colorBoxListはforの引数でしか呼ばれないので、変数に出さずに書いてみよう by yuki.wakisaka (2017/04/30)
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd");
+        List<String> date = newArrayList();
+        for (ColorBox colorBox : getColorBoxList()) {
             List<BoxSpace> spaceList = colorBox.getSpaceList();
             for (BoxSpace boxSpace : spaceList) {
                 Object contents = boxSpace.getContents();
                 if (contents instanceof LocalDate) {
                     // done sawa 毎回この形式に合わせるの、きっといい感じのclassがあるよ by yuki.wakisaka (2017/04/30)
                     // [コメント] ＼(^o^)／ by sawa
-                    // TODO sawa FormatterはLocalDateでもLocalDateTimeでも使いまわすので、外で定義しちゃうのがいいかな。
+                    date.add(((LocalDate) contents).format(format));
+                    // TODO done sawa FormatterはLocalDateでもLocalDateTimeでも使いまわすので、外で定義しちゃうのがいいかな。
                     // 今回の出力形式を定義するという意味では、1行目とかでもいいくらい。 by yuki.wakisaka (2017/05/01)
-                    String date = ((LocalDate) contents).format(DateTimeFormatter.ofPattern("MM/dd"));
-                    // TODO sawa log(...) はfor文の外に出しておこう。処理の切り分け。 by yuki.wakisaka (2017/05/01)
-                    log("カラーボックスに入っている日付は" + date + "です");
+                    // TODO done sawa log(...) はfor文の外に出しておこう。処理の切り分け。 by yuki.wakisaka (2017/05/01)
                 } else if (contents instanceof LocalDateTime) {
-                    String date  = ((LocalDateTime) contents).format(DateTimeFormatter.ofPattern("MM/dd"));
-                    log("カラーボックスに入っている日付は" + date + "です");
+                    date.add(((LocalDateTime) contents).format(format));
                 }
             }
         }
+        log("カラーボックスに入っている日付は");
+        for (String dateContent: date) {
+            log(dateContent);
+        }
+        log("です");
     }
 
     // ===================================================================================
@@ -65,23 +69,27 @@ public class SawaQ2DateTest extends ColorBoxTestCase {
      */
     public void test_weekOfDay() {
         List<ColorBox> colorBoxList = getColorBoxList();
+        LocalDate date = null;
         // done sawa for文では日付の抽出だけをして、最後にlogを書き出すときに曜日にしよう。機能の切り分け。 by yuki.wakisaka (2017/04/30)
-        // TODO ↑ log(...)はfor文の外に出しちゃおう、の意も含む！ by yuki.wakisaka (2017/05/01)
+        // TODO done ↑ log(...)はfor文の外に出しちゃおう、の意も含む！ by yuki.wakisaka (2017/05/01)
         for (ColorBox colorBox : colorBoxList) {
             List<BoxSpace> spaceList = colorBox.getSpaceList();
             for (BoxSpace boxSpace : spaceList) {
                 Object contents = boxSpace.getContents();
                 // done sawa ここで宣言する意味ないな by yuki.wakisaka (2017/04/30)
                 if (contents instanceof LocalDate) {
-                    LocalDate date = ((LocalDate) contents);
-                    log("カラーボックスに入っている最初の日付の曜日は" + date.getDayOfWeek().getDisplayName(TextStyle.FULL, JAPAN) + "です");
+                    date = ((LocalDate) contents);
                     break;
                 } else if (contents instanceof LocalDateTime) {
-                    LocalDateTime date = ((LocalDateTime) contents);
-                    log("カラーボックスに入っている最初の日付の曜日は" + date.getDayOfWeek().getDisplayName(TextStyle.FULL, JAPAN) + "です");
+                    date = ((LocalDateTime) contents).toLocalDate();
                     break;
                 }
             }
+        }
+        if (date != null) {
+            log("カラーボックスに入っている最初の日付の曜日は" + date.getDayOfWeek().getDisplayName(TextStyle.FULL, JAPAN) + "です");
+        } else {
+            log("カラーボックスに日付は入っていません");
         }
     }
 
@@ -94,7 +102,7 @@ public class SawaQ2DateTest extends ColorBoxTestCase {
             // TODO done sawa "yellow".equals(...)の方が良い
             // 理由を考えてみて、コメントに残してくださいなー by yuki.wakisaka (2017/04/30)
             // TODO [コメント] "yello"が先にくれば、colorBox.getColor().getColorName()で色の名前を取り出していることがすぐにわかるから(?) by sawa (2017/05/01)
-            // TODO sawa ↑ この問題に限らず、文字列比較する際は定数を先に持ってきたほうがいい、という意味合いですね。 "hoge".equals(someStr) みたいな
+            // TODO done sawa ↑ この問題に限らず、文字列比較する際は定数を先に持ってきたほうがいい、という意味合いですね。 "hoge".equals(someStr) みたいな
             // もっと、実害がでうるかも..というところの理由があります！ by yuki.wakisaka (2017/05/01)
             if (("yellow").equals(colorBox.getColor().getColorName())) {
                 List<BoxSpace> spaceList = colorBox.getSpaceList();
@@ -118,16 +126,17 @@ public class SawaQ2DateTest extends ColorBoxTestCase {
      * 来年(2018年)の新卒が入社する日は何曜日？
      */
     public void test_weekOfDayOf2017Newcomer() {
-        LocalDate entryDay = LocalDate.of(2018, 4, 1);
-        // TODO sawa entryWeekでは「入社する週」なので entryDayOfWeekのがいいすね by yuki.wakisaka (2017/05/01)
-        // TODO これをStringではなくDayOfWeekで持てば、比較するときも楽 by yuki.wakisaka (2017/05/01)
-        String entryWeek = entryDay.getDayOfWeek().getDisplayName(TextStyle.FULL, JAPAN);
+        LocalDate entryDayOfWeek = LocalDate.of(2018, 4, 1);
+        // TODO done sawa entryWeekでは「入社する週」なので entryDayOfWeekのがいいすね by yuki.wakisaka (2017/05/01)
+        // TODO done これをStringではなくDayOfWeekで持てば、比較するときも楽 by yuki.wakisaka (2017/05/01)
+        DayOfWeek entryWeek = entryDayOfWeek.getDayOfWeek();
         // done sawa 文字列ではなく、DayOfWeekで比較したほうが安全（タイポとか、TextStyleの差異とか） by yuki.wakisaka (2017/04/30)
-        if (entryDay.getDayOfWeek().equals(DayOfWeek.SATURDAY) || entryDay.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
-            // TODO sawa ここでentryWeekに月曜を再代入してlogに出すほうが、出力形式にブレが減る by yuki.wakisaka (2017/05/01)
-            log("月曜日です");
+        if (entryWeek.equals(DayOfWeek.SATURDAY) || entryWeek.equals(DayOfWeek.SUNDAY)) {
+            // TODO done sawa ここでentryWeekに月曜を再代入してlogに出すほうが、出力形式にブレが減る by yuki.wakisaka (2017/05/01)
+            entryWeek = DayOfWeek.MONDAY;
+            log("来年(2018年)の新卒が入社する日は" + entryWeek.getDisplayName(TextStyle.FULL, JAPAN) + "です");
         } else {
-            log(entryWeek + "です");
+            log("来年(2018年)の新卒が入社する日は" + entryWeek.getDisplayName(TextStyle.FULL, JAPAN) + "です");
         }
     }
 
@@ -156,10 +165,13 @@ public class SawaQ2DateTest extends ColorBoxTestCase {
                         date.add(((LocalDateTime) contents).toLocalDate());
                     }
                 }
-                // TODO sawa 1つの場合のlogも出そう。& 3つ以上の場合は？ by yuki.wakisaka (2017/05/01)
-                if (date.size() >= 2) {
+                // TODO done sawa 1つの場合のlogも出そう。& 3つ以上の場合は？ by yuki.wakisaka (2017/05/01)
+                // TODO [コメント] 日付が二つの場合以外は問題の要件を満たさないので、elseを通るようにしました！ by sawa (2017/05/01)
+                if (date.size() == 2) {
                     long gap = ChronoUnit.DAYS.between(date.get(1), date.get(0));
                     log("二つの日付の日数の差は" + (gap) + "日です");
+                } else {
+                    log("yellowに入っている日付は2つではありません");
                 }
             }
         }
