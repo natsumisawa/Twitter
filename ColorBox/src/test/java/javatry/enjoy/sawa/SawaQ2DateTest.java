ede4caaacdd81611397bdc44c2fa4bef693e5c9c
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2017-05-16 22:09:05 +0900

    rev date by ukwksk

--- ColorBox/src/test/java/javatry/enjoy/sawa/SawaQ2DateTest.java
+++ ColorBox/src/test/java/javatry/enjoy/sawa/SawaQ2DateTest.java
@@ -56,6 +56,7 @@ public class SawaQ2DateTest extends ColorBoxTestCase {
                     TemporalAccessor content = (TemporalAccessor) contents;
                     if (content.isSupported(ChronoField.MONTH_OF_YEAR) && content.isSupported(ChronoField.DAY_OF_WEEK)) {
                         // done よく頑張りました！が、インデント..詰めの甘さよw by yuki.wakisaka (2017/05/12)
+                        // TODO sawa 不要なキャスト by yuki.wakisaka (2017/05/16)
                         dateList.add((TemporalAccessor) content);
                     }
                     // done sawa 毎回この形式に合わせるの、きっといい感じのclassがあるよ by yuki.wakisaka (2017/04/30)
@@ -203,6 +204,7 @@ public class SawaQ2DateTest extends ColorBoxTestCase {
                 // [コメント] 日付が二つの場合以外は問題の要件を満たさないので、elseを通るようにしました！ by sawa (2017/05/01)
                 // [コメント] おけ、それでいいと思います！ by ukwksk (2017/05/01)
                 if (date.size() == 2) {
+                    // TODO sawa 日数の差が負数になる場合があるので、絶対値をとりましょう by yuki.wakisaka (2017/05/16)
                     long gap = ChronoUnit.DAYS.between(date.get(1), date.get(0));
                     log("二つの日付の日数の差は" + (gap) + "日です");
                 } else {
