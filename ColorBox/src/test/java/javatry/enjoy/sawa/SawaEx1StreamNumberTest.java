cf71c8a0b50b5edbf33ec1c5ae7e76faa1b194f7
natsumi.sawa <natsumi.sawa@bizreach.co.jp>
2017-05-29 22:14:01 +0900

    streamNumberTest

--- ColorBox/src/test/java/javatry/enjoy/sawa/SawaEx1StreamNumberTest.java
+++ ColorBox/src/test/java/javatry/enjoy/sawa/SawaEx1StreamNumberTest.java
@@ -1,19 +1,14 @@
 package javatry.enjoy.sawa;
 
-import javatry.colorbox.ColorBox;
 import javatry.colorbox.space.BoxSpace;
 import javatry.colorbox.unit.ColorBoxTestCase;
-import jdk.internal.util.xml.impl.Pair;
-import org.omg.CORBA.IMP_LIMIT;
-import org.omg.CORBA.MARSHAL;
-import sun.jvm.hotspot.jdi.IntegerTypeImpl;
 
 import java.math.BigDecimal;
-import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
+import java.util.Map;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.List;
+import java.util.Collection;
 /**
  * 数値関連のテスト。<br>
  * 何々は？と言われたら、それに該当するものをログに出力すること。
@@ -27,46 +22,68 @@ public class SawaEx1StreamNumberTest extends ColorBoxTestCase {
      * 青色のカラーボックスに入ってる Map の中の商品で一番高いものは？
      */
     public void test_findMax() {
-//        Optional<String> blue = getColorBoxList().stream().filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
-//                .flatMap(colorBox -> colorBox.getSpaceList().stream().map(BoxSpace::getContents)
-//                        .filter(obj -> obj instanceof Map<?, ?>))
-//                .map(map -> ((Map<?, ?>) map).entrySet().stream())
-//                .filter(m -> m.mapToInt(map -> (Integer) map.getValue()).max().isPresent())
-//                .flatMap(setm -> setm.map(map -> (String) map.getKey())).findFirst();
-//        log(blue.get());
-        Optional<? extends Map.Entry<?, ?>> max = getColorBoxList().stream()//.filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
-                .flatMap(colorBox -> colorBox.getSpaceList().stream().map(BoxSpace::getContents)
+        String log = getColorBoxList().stream().filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
+                .flatMap(colorBox -> colorBox.getSpaceList().stream()
+                        .map(BoxSpace::getContents)
                         .filter(obj -> obj instanceof Map<?, ?>))
-                .flatMap(map -> ((Map<?, ?>) map).entrySet().stream())//ここまででmapが全部とれてるよ
-                .max(Comparator.comparing(entry -> (Integer) entry.getValue()));
-        log(max.get().getKey());
-        //.flatMap(map -> ((Map<?, ?>) map).entrySet().stream().map(Map.Entry::getKey)).findFirst();
-        //log(maxContent.get());
-
-//        getColorBoxList().stream().filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
-//                .flatMap(colorBox -> colorBox.getSpaceList().stream().map(BoxSpace::getContents)
-//                        .filter(obj -> obj instanceof Map<?, ?>)
-//                        .flatMap(map -> ((Map<?, ?>) map).entrySet().stream().map(Map.Entry::getValue))).max(Comparator.comparing(Object::toString));
-
-//        Stream<Object> mapContents = getColorBoxList().stream().filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
-//                .flatMap(colorBox -> colorBox.getSpaceList().stream().map(boxSpace ->
-//                        boxSpace.getContents()).filter(object ->
-//                        object instanceof Map<?, ?>));
-//        Object o = mapContents.collect(Collectors.toList()).get(0);
-//        (Map<?, ?>)o.entrySet().
-//        log(blue.collect(Collectors.toList()).get(0));
-//        getColorBoxList().stream().map(colorBox -> colorBox.getSpaceList()
-//        .stream().map(BoxSpace::getContents))
-//        .forEach(objectStream -> objectStream
-//        .forEach(System.out::println));
+                .flatMap(map -> ((Map<?, ?>) map).entrySet().stream())
+                .max(Comparator.comparing(entry -> (Integer) entry.getValue()))
+                .map(entry -> "青色のカラーボックスに入ってる Map の中の商品で一番高いものは" + entry.getKey() + "です")
+                .orElse("青色のカラーボックスに入ってる Map はありません");
+        log(log);
+        //------------思い出コード------------------------------
+        //getColorBoxList().stream().filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
+        //        .flatMap(colorBox -> colorBox.getSpaceList().stream().map(BoxSpace::getContents)
+        //                .filter(obj -> obj instanceof Map<?, ?>)
+        //                .flatMap(map -> ((Map<?, ?>) map).entrySet().stream().map(Map.Entry::getValue))).max(Comparator.comparing(Object::toString));
+        //
+        //Stream<Object> mapContents = getColorBoxList().stream().filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
+        //        .flatMap(colorBox -> colorBox.getSpaceList().stream().map(boxSpace ->
+        //                boxSpace.getContents()).filter(object ->
+        //                object instanceof Map<?, ?>));
+        //Object o = mapContents.collect(Collectors.toList()).get(0);
+        //(Map<?, ?>)o.entrySet().
+        //log(blue.collect(Collectors.toList()).get(0));
+        //getColorBoxList().stream().map(colorBox -> colorBox.getSpaceList()
+        //.stream().map(BoxSpace::getContents))
+        //.forEach(objectStream -> objectStream
+        //.forEach(System.out::println));
     }
 
     /**
      * カラーボックスの中に入ってる BigDecimal を全て足し合わせると？
      */
     public void test_sumBigDecimal() {
-        getColorBoxList().stream().flatMap(colorBox -> colorBox.getSpaceList().stream().map(BoxSpace::getContents))
-                .filter(object -> object instanceof List).map(Object -> (List) Object).map(listcontent -> listcontent instanceof BigDecimal).map(cont -> (BigDecimal)cont).reduce((r1,r2) -> r1.add(r2));
-//        bd.ifPresent(this::log);
+
+        Optional<BigDecimal> listBdOpt = getColorBoxList().stream().flatMap(colorBox -> colorBox.getSpaceList().stream())
+                .map(BoxSpace::getContents)
+                .filter(content -> content instanceof List)
+                .map(listContent -> (List) listContent)
+                .flatMap(Collection::stream)
+                .filter(list -> list instanceof BigDecimal)
+                .map(content -> (BigDecimal) content)
+                .reduce(BigDecimal::add);
+        Optional<BigDecimal> bdOpt = getColorBoxList().stream().flatMap(colorBox -> colorBox.getSpaceList().stream())
+                .map(BoxSpace::getContents)
+                .filter(contents -> contents instanceof BigDecimal)
+                .map(bdContent -> (BigDecimal) bdContent)
+                .reduce(BigDecimal::add);
+        BigDecimal answer = getAnswer(listBdOpt, bdOpt);
+        if (answer == null) {
+            log("カラーボックスの中に BigDecimal は入っていません");
+        } else {
+            log("カラーボックスの中に入ってる BigDecimal を全て足し合わせると" + answer + "です");
+        }
+    }
+
+    private BigDecimal getAnswer(Optional<BigDecimal> listBdOpt, Optional<BigDecimal> bdOpt) {
+        if (!listBdOpt.isPresent() && !bdOpt.isPresent()) {
+            return null;
+        } else if(listBdOpt.isPresent() && !bdOpt.isPresent()) {
+            return listBdOpt.get();
+        } else if(!listBdOpt.isPresent()) {
+            return bdOpt.get();
+        }
+        return listBdOpt.get().add(bdOpt.get());
     }
 }
