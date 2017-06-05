303dccc9830c1a8052921dd417e0fa06d41d753c
natsumi.sawa <natsumi.sawa@bizreach.co.jp>
2017-06-05 10:47:52 +0900

    デビル

--- ColorBox/src/test/java/javatry/enjoy/sawa/test.java
+++ ColorBox/src/test/java/javatry/enjoy/sawa/test.java
@@ -12,17 +12,26 @@ import java.util.Map;
 public class test extends ColorBoxTestCase {
 
     public void test_findMax() {
-        String log1 = getColorBoxList().stream()
-            .filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
-            .flatMap(colorBox -> colorBox.getSpaceList().stream()
-                .map(BoxSpace::getContents)
-                .filter(obj -> obj instanceof Map<?, ?>)
-                .map(o -> (Map<?, ?>) o))
-            .flatMap(map -> ((Map<?, ?>) map).entrySet().stream())
-            .max(Comparator.comparing(entry -> (Integer) entry.getValue()))
-            .map(entry -> "青色のカラーボックスに入ってる Map の中の商品で一番高いものは" + entry.getKey() + "です")
-            .orElse("青色のカラーボックスに入ってる Map はありません");
-        log(log1);
+        getColorBoxList().stream()
+            .filter(colorBox -> getColorBoxList().stream()
+                    .flatMap(box -> box.getSpaceList().stream())
+                    .map(BoxSpace::getContents)
+                    .filter(obj -> obj instanceof Map<?, ?>)
+                    .map(o -> (Map<?, ?>) o)
+                    .flatMap(map -> ((Map<?, ?>) map).entrySet().stream())
+                    .filter(entry -> entry.getValue() instanceof Number)
+                    .max(Comparator.comparing(entry -> ((Number) (entry.getValue())).intValue())).isPresent()
+            ).
+
+//                .map(BoxSpace::getContents)
+//                .filter(obj -> obj instanceof Map<?, ?>)
+//                .map(o -> (Map<?, ?>) o)
+//                .flatMap(map -> ((Map<?, ?>) map).entrySet().stream())
+//                    .max(Comparator.comparing(entry -> (Integer) entry.getValue()))
+//
+//            .map(entry -> "Map の中の商品で一番高いものが入っているカラーボックスは" + entry.getKey() + "です")
+//            .orElse("青色のカラーボックスに入ってる Map はありません");
+//        log(log1);
 
         String log2 = getColorBoxList().stream()
             .filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
