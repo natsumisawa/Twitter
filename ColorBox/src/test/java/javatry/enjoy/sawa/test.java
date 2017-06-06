bda2730dee4db354b2a8a74f4be345aab9a36851
natsumi.sawa <natsumi.sawa@bizreach.co.jp>
2017-06-06 10:20:58 +0900

    テスト

--- ColorBox/src/test/java/javatry/enjoy/sawa/test.java
+++ ColorBox/src/test/java/javatry/enjoy/sawa/test.java
@@ -5,6 +5,7 @@ import javatry.colorbox.unit.ColorBoxTestCase;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by natsumi.sawa on 2017/06/01.
@@ -12,26 +13,6 @@ import java.util.Map;
 public class test extends ColorBoxTestCase {
 
     public void test_findMax() {
-        getColorBoxList().stream()
-            .filter(colorBox -> getColorBoxList().stream()
-                    .flatMap(box -> box.getSpaceList().stream())
-                    .map(BoxSpace::getContents)
-                    .filter(obj -> obj instanceof Map<?, ?>)
-                    .map(o -> (Map<?, ?>) o)
-                    .flatMap(map -> ((Map<?, ?>) map).entrySet().stream())
-                    .filter(entry -> entry.getValue() instanceof Number)
-                    .max(Comparator.comparing(entry -> ((Number) (entry.getValue())).intValue())).isPresent()
-            ).
-
-//                .map(BoxSpace::getContents)
-//                .filter(obj -> obj instanceof Map<?, ?>)
-//                .map(o -> (Map<?, ?>) o)
-//                .flatMap(map -> ((Map<?, ?>) map).entrySet().stream())
-//                    .max(Comparator.comparing(entry -> (Integer) entry.getValue()))
-//
-//            .map(entry -> "Map の中の商品で一番高いものが入っているカラーボックスは" + entry.getKey() + "です")
-//            .orElse("青色のカラーボックスに入ってる Map はありません");
-//        log(log1);
 
         String log2 = getColorBoxList().stream()
             .filter(colorBox -> colorBox.getColor().getColorName().equals("blue"))
